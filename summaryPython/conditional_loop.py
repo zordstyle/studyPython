e9ae6052d33118ee17febc89ec0964fc3023b9fc
zordstyle <bkbc0513@gmail.com>
2022-06-13 08:35:17 +0900

    from function

--- summaryPython/conditional_loop.py
+++ summaryPython/conditional_loop.py
@@ -123,3 +123,96 @@ while i <= 9:
     i += 1
 
 print(result)
+
+# 1부터 9까지 홀수의 합 구하기 예제(while문)
+i = 1
+result = 0
+
+# i가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+    if i % 2 == 1:
+        result += i
+    i += 1
+
+print(result)
+
+# 반복문에서의 무한 루프
+# 무한 루프(Infinite Loop) : 끊임없이 반복되는 반복 구문을 의미합니다.
+# 코딩 테스트에서 무한 루프를 구현할 일은 거의 없으니 유의.
+# 반복문을 작성한 뒤에는 항상 반복문을 탈출할 수 있는지 확인.
+
+# 반복문 For
+# for문의 구조는 특정한 변수를 이용하여 'in' 뒤에 오는 데이터(리스트, 튜플 등)에
+# 포함되어 있는 원소를 첫 번째 인덱스부터 차례대로 하나씩 방문한다.
+# for 변수 in 리스트:
+#     실행할 소스코드
+array = [9, 8, 7, 6, 5]
+for x in array:
+    print(x)
+
+# for문에서 연속적인 값을 차례대로 순회할 때는 range()를 주로 사용한다.
+# 이 때 range(시작 값, 끝 값 +1)형태로 사용.
+# 인자를 하나만 넣으면 자동으로 시작 값은 0이 된다.
+
+result = 0
+
+# i는 1부터 9까지의 모든 값을 순회
+for i in range(1, 10):
+    result += i
+
+print(result)
+
+# 1부터 30까지 모든 정수의 합 구하기 예제(for문)
+result = 0
+for i in range(1, 31):
+    result += i
+
+print(result)
+
+# continue : 반복문에서 남은 코드의 실행을 건너뛰고, 다음 반복을 진행하고자 할 때 사용.
+
+# 1부터 9까지의 홀수의 합(continue)
+result = 0
+
+for i in range(1, 10):
+    if i % 2 == 0:
+        continue
+    result += i
+
+print(result)
+
+# break : 반복문을 즉시 탈출
+# 1부터 5까지의 정수를 차례대로 출력
+i = 1
+
+while True:
+    print("현재 i의 값 :", i)
+    if i == 5:
+        break
+    i += 1
+
+# 예제
+# 1. 학생들의 합격 여부 판단 예제 : 점수가 80점이 넘으면 합격
+
+score = [90, 85, 77, 65, 97]
+
+for i in range(5):
+    if score[i] >= 80:
+        print(i + 1, "번 학생은 합격입니다.")
+
+# 2. 학생들의 합격 여부 판단 예제 : 특정 번호의 학생은 제외하기
+
+score = [90, 85, 77, 65, 97]
+cheating_student_list = {2, 4}
+
+for i in range(5):
+    if i+1 in cheating_student_list:
+        continue
+    if score[i] >= 80:
+        print(i + 1, "번 학생은 합격입니다.")
+
+# 3. 중첩 반복문 : 구구단 예제
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(i, "X", j, "=", i*j)
+    print() # 단 별 나누기 위한 것(별거아님)
