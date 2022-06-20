a0d2d13ff653538de1d9e0b01f773a64744b002f
zordstyle <bkbc0513@gmail.com>
2022-06-20 23:05:14 +0900

    start baekjoon judge

---
+++ Beakjoon_Basic/IO_and_Arithmetic_Opreation/1000.py
@@ -0,0 +1,38 @@
+# input() : 입력된 값 문자열로 인식
+# 괄호 안에 문자열 넣으면 인터페이스로 활용
+# N = input("당신의 이름은...?: ")
+
+# 입력값을 숫자로 : int 함수 사용
+# N = int(input("입력 : "))
+
+# 한 줄에 입력값 두 개 이상 구분 : split() 함수 사용
+# split() : 띄어 쓰기를 기준으로 한 문자열을 나누어 리스트로 구분해준다.
+# a = "This is baekjoon's homepage"
+# print(a.split()) # ['This', 'is', "baekjoon's", 'homepage']
+
+# input()와 split()을 결합하면?
+# N = input().split() # 입력값 : 11 12 13 14
+# print(N) # 출력값 : ['11', '12', '13', '14']
+
+# 입력값 두 개 이상으로 구분
+# A, B = input().split() # 입력값 : 11, 12
+# print(A) # 11
+# print(B) # 12
+
+# 두 수를 입력받아 합을 출력
+# A, B = input().split()
+# x = int(A)
+# y = int(B)
+
+# int() 함수는 리스트를 정수형으로 변경할 수 없다. 또한 int()는 하나의 값에만 적용 가능.
+# A, B = int(input().split()) # 에러
+
+# map() 함수
+# 사용법 : map(적용할 함수, 반복 가능한 자료형)
+# A, B = map(int, ['231', '23234'])
+# print(A, B) # 231 23234
+
+# 두 수를 입력 받아 합을 출력 (map 함수 사용)
+a, b = map(int, input().split())
+print(a+b)
+
